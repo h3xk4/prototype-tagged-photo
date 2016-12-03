9eb4f0e17ef2101358dee978ee80bc872b19b6d2
h3xk4 <michael@metaset.co>
2016-12-03 23:53:23 +1100

    Tags working 💊

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { browserHistory, Router, Route, Link, History } from 'react-router'
+const canvas = document.querySelector('.tagged');
+const hero = document.querySelector('.hero');
 
-// import NotFound from './ui/404'
 
-import HelloWorld from './ui/hello-world'
+/**
+ * Add marker
+ */
 
-ReactDOM.render(
-  <HelloWorld />,
-  document.getElementById('content')
-);
+canvas.addEventListener('click', function(e) {
+
+  const xWidth = hero.width;
+  const yHeight = hero.height;
+
+  const xPosition = e.pageX - this.offsetLeft;
+  const yPosition = e.pageY - this.offsetTop;
+
+  const x = (xPosition / xWidth) * 100;
+  const y = (yPosition / yHeight) * 100;
+
+  const newMarker = document.createElement("span");
+
+  newMarker.classList.add("marker");
+  newMarker.style.left = (x - 0.5)+ "%";
+  newMarker.style.top = (y - 1) + "%";
+
+  canvas.appendChild(newMarker);
+})
