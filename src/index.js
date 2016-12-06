da9af6a53e9b96f753d881c6e53a4db12b0d33c8
h3xk4 <michael@metaset.co>
2016-12-07 00:21:35 +1100

    Start converting to React component.

--- src/index.js
+++ src/index.js
@@ -1,107 +1,90 @@
-var forEach = function (array, callback, scope) {
-  for (var i = 0; i < array.length; i++) {
-    callback.call(scope, i, array[i]);
-  }
-};
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { browserHistory, Router, Route, Link, History } from 'react-router'
 
-
-/**
- * tagged image with notes
- */
-
-const data = []
-const canvas = document.querySelector('.tagged');
-const hero = document.querySelector('.hero');
-
+const data = require('./data.json');
+const hero = document.querySelector('.hero_image');
+const heroCanvas = document.querySelector('.hero_canvas');
 
 /**
- * Create a new marker
+ * Note
  */
 
-function addMarker(e) {
+class Notes extends React.Component {
 
-  // Position calculations
-  const xWidth = hero.width;
-  const yHeight = hero.height;
-  const xPosition = e.pageX - this.offsetLeft;
-  const yPosition = e.pageY - this.offsetTop;
-  const x = (xPosition / xWidth) * 100;
-  const y = (yPosition / yHeight) * 100;
-
-  // Create new marker element
-  const newMarker = document.createElement('div');
-
-  newMarker.classList.add('note_marker');
-  newMarker.style.left = (x - 0.5)+ '%';
-  newMarker.style.top = (y - 1) + '%';
-
-  // Create the marker content
-  const noteContent = document.createElement('div');
-  noteContent.classList.add('note_content');
-
-  const noteContentInput = document.createElement('textarea');
-  //noteContentInput.setAttribute('type', 'text');
-  noteContentInput.classList.add('note_input');
-
-  // Build the marker
-  noteContent.appendChild(noteContentInput);
-  newMarker.appendChild(noteContent);
-  canvas.appendChild(newMarker);
-
-  // Create the marker data object
-  const markerData = {
-    id: data.length,
-    x: x,
-    y: y,
-    text: noteContentInput.value
+  constructor() {
+    super();
+    this.newNote = this.newNote.bind(this);
   }
 
-  // Push marker data to data object
-  data.push(markerData);
+  newNote(e) {
 
-  // Add marker data to local storage
-  localStorage.setItem('data', JSON.stringify(data));
+    console.log('New note');
+
+    // Position calculations
+    const xWidth = hero.width;
+    const yHeight = hero.height;
+    const xPosition = e.pageX - hero.offsetLeft;
+    const yPosition = e.pageY - hero.offsetTop;
+    const x = (xPosition / xWidth) * 100;
+    const y = (yPosition / yHeight) * 100;
+
+    // Create new marker element
+    const newMarker = document.createElement('div');
+
+    newMarker.classList.add('note_marker');
+    newMarker.style.left = (x - 0.5)+ '%';
+    newMarker.style.top = (y - 1) + '%';
+
+    // Create the marker content
+    const noteContent = document.createElement('div');
+    noteContent.classList.add('note_content');
+
+    const noteContentInput = document.createElement('textarea');
+    //noteContentInput.setAttribute('type', 'text');
+    noteContentInput.classList.add('note_input');
+
+    // Build the marker
+    noteContent.appendChild(noteContentInput);
+    newMarker.appendChild(noteContent);
+    heroCanvas.appendChild(newMarker);
+
+    // Create the marker data object
+    const markerData = {
+      id: data.length,
+      x: x,
+      y: y,
+      text: noteContentInput.value
+    }
+
+    // Push marker data to data object
+    data.push(markerData);
+
+    // Add marker data to local storage
+    localStorage.setItem('data', JSON.stringify(data));
+  }
+
+  render() {
+    return (
+      <div className="hero_notes" onClick={this.newNote}></div>
+    )
+  }
 }
 
 
 /**
- * Add a marker
+ * App
  */
 
-canvas.addEventListener('click', addMarker, false);
+class App extends React.Component {
 
-
-/**
- * Is note content showing?
- * If yes, then disable addMarker()
- */
-
-canvas.addEventListener('mouseover', function(e) {
-
-  // get hovered element
-  const el = e.target.className;
-
-  // Only allow new markers if hover is hero image
-  if (el == 'hero') { this.addEventListener('click', addMarker, false); }
-  else { this.removeEventListener('click', addMarker, false); }
-});
-
-
-/**
- * Reset notes
- */
-
-const reset = document.querySelector('.reset');
-
-reset.addEventListener('click', resetNotes, false);
-
-function resetNotes() {
-  localStorage.clear();
-  const notes = document.querySelectorAll('.note_marker');
-  forEach(notes, function (index, el) {
-    el.remove();
-  });
+  render() {
+    return (
+      <Notes />
+    )
+  }
 }
 
-
-
+ReactDOM.render(
+  <App />, document.querySelector('.hero_canvas')
+);
